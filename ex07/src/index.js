1e2c325c214687e671c93e31d35bdd665e45b302
[Bajro] <[b.dunic@hotmail.com]>
2021-09-20 22:18:04 +0200

    exercise day08

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -3,13 +3,13 @@ var sum = 0;
 function addThree() {
 
     sum = sum + 3;
-
+    console.log(sum);
 }
 
 function addFive() {
 
     sum = sum + 5;
-    
+    console.log(sum);
 }
 
 console.log(addThree());
